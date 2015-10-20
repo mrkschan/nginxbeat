cefd90b736f61bdcfee2b180d96138d3249c61b5
KS Chan <mrkschan@gmail.com>
2015-10-21 00:14:32 +0800

    Parse config from nginxbeat.yml

--- config.go
+++ config.go
@@ -1,6 +1,13 @@
 package main
 
 type NginxConfig struct {
+	// URL to Nginx monitoring page.
+	// Defaults to "http://127.0.0.1/status".
+	URL string
+
+	// Period defines how often to read status in seconds.
+	// Defaults to 1 second.
+	Period *int64
 }
 
 type ConfigSettings struct {
--- nginxbeat.go
+++ nginxbeat.go
@@ -1,30 +1,71 @@
 package main
 
 import (
+	"net/url"
+	"time"
+
 	"github.com/elastic/libbeat/beat"
+	"github.com/elastic/libbeat/cfgfile"
+	"github.com/elastic/libbeat/logp"
 )
 
 // Nginxbeat implements Beater interface and sends Nginx status using libbeat.
 type Nginxbeat struct {
-	config ConfigSettings
+	// NbConfig holds configurations of Nginxbeat parsed by libbeat.
+	NbConfig ConfigSettings
+
+	url    *url.URL
+	period time.Duration
 }
 
 // Config Nginxbeat according to nginxbeat.yml.
 func (nb *Nginxbeat) Config(b *beat.Beat) error {
+	err := cfgfile.Read(&nb.NbConfig, "")
+	if err != nil {
+		logp.Err("Error reading configuration file: %v", err)
+		return err
+	}
+
+	var u string
+	if nb.NbConfig.Input.URL != "" {
+		u = nb.NbConfig.Input.URL
+	} else {
+		u = "http://127.0.0.1/status"
+	}
+	nb.url, err = url.Parse(u)
+	if err != nil {
+		logp.Err("Invalid Nginx status page: %v", err)
+		return err
+	}
+
+	if nb.NbConfig.Input.Period != nil {
+		nb.period = time.Duration(*nb.NbConfig.Input.Period) * time.Second
+	} else {
+		nb.period = 1 * time.Second
+	}
+
+	logp.Debug("nginxbeat", "Init nginxbeat")
+	logp.Debug("nginxbeat", "Watch %v\n", nb.url)
+	logp.Debug("nginxbeat", "Period %v\n", nb.period)
+
+	return nil
 }
 
 // Setup Nginxbeat.
 func (nb *Nginxbeat) Setup(b *beat.Beat) error {
+	return nil
 }
 
 // Run Nginxbeat.
 func (nb *Nginxbeat) Run(b *beat.Beat) error {
+	return nil
 }
 
 // Cleanup Nginxbeat.
 func (nb *Nginxbeat) Cleanup(b *beat.Beat) error {
+	return nil
 }
 
 // Stop Nginxbeat.
-func (nb *Nginxbeat) Stop(b *beat.Beat) error {
+func (nb *Nginxbeat) Stop() {
 }
