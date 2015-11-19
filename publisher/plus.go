3f822f6f4f7fbcedbd03b51c28879b3a96e4091a
KS Chan <mrkschan@gmail.com>
2015-11-19 22:16:01 +0800

    Publish Nginx Plus upstream, zone, cache, and stream with individual document types

--- publisher/plus.go
+++ publisher/plus.go
@@ -19,9 +19,51 @@ func NewPlusPublisher(c publisher.Client) *PlusPublisher {
 
 // Publish Nginx Plus status.
 func (p *PlusPublisher) Publish(s map[string]interface{}) {
+	zones := s["server_zones"].([]interface{})
+	delete(s, "server_zones")
+
+	upstreams := s["upstreams"].([]interface{})
+	delete(s, "upstreams")
+
+	caches := s["caches"].([]interface{})
+	delete(s, "caches")
+
+	stream := s["stream"]
+	delete(s, "stream")
+
 	p.client.PublishEvent(common.MapStr{
 		"@timestamp": common.Time(time.Now()),
 		"type":       "nginx",
 		"nginx":      s,
+	})
+
+	for _, i := range zones {
+		p.client.PublishEvent(common.MapStr{
+			"@timestamp": common.Time(time.Now()),
+			"type":       "zone",
+			"zone":       i,
+		})
+	}
+
+	for _, i := range upstreams {
+		p.client.PublishEvent(common.MapStr{
+			"@timestamp": common.Time(time.Now()),
+			"type":       "upstream",
+			"upstream":   i,
+		})
+	}
+
+	for _, i := range caches {
+		p.client.PublishEvent(common.MapStr{
+			"@timestamp": common.Time(time.Now()),
+			"type":       "cache",
+			"cache":      i,
+		})
+	}
+
+	p.client.PublishEvent(common.MapStr{
+		"@timestamp": common.Time(time.Now()),
+		"type":       "stream",
+		"stream":     stream,
 	})
 }
